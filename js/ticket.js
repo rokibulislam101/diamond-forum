743a04e52006d50cb5830281010886ab04e572e0
MD Rokibul Islam Mollah <rokibulislamalways101@gmail.com>
2024-03-04 03:38:37 +0600

    Third Commit

--- js/ticket.js
+++ js/ticket.js
@@ -1,3 +1,54 @@
+// Discuss Function Section--
+fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
+  .then(response => {
+    return response.json();
+  })
+  .then(postsData => {
+    let postsHTML = '';
+    postsData.posts.map(post => {
+      postsHTML += `<div class="flex bg-neutral-100 m-5 p-5 rounded-2xl gap-5">
+          <div class="relative">
+            <img src="${
+              post.image
+            }" alt="img" class="profile-img w-20 rounded-3xl" />
+            ${
+              post.isActive
+                ? '<div class="absolute top-0 right-0 w-4 h-4 bg-green-500 rounded-full"></div>'
+                : '<div class="absolute top-0 right-0 w-4 h-4 bg-red-500 rounded-full"></div>'
+            }
+          </div>
+          <div class="w-full">
+            <div class="flex gap-4 pb-2">
+              <p>#${post.category}</p>
+              <p>Author: ${post.author.name || 'Unknown'}</p>
+            </div>
+            <h2 class="font-bold py-2">${post.title}</h2>
+            <p class="py-2">${post.description}</p>
+            <div class="flex justify-between items-center">
+              <div class="flex gap-5">
+                <p><i class="fa-regular fa-message me-3"></i>${
+                  post.comment_count
+                }</p>
+                <p><i class="fa-regular fa-eye me-3"></i>${post.view_count}</p>
+                <p><i class="fa-regular fa-clock me-3"></i>${
+                  post.posted_time
+                } min</p>
+              </div>
+              <button class="bg-green-400 text-white p-2 flex items-center rounded-full me-0"><i class="fa-regular fa-envelope-open"></i></button>
+            </div>
+  
+
+          </div>
+        </div>`;
+    });
+    document.getElementById('posts').innerHTML = postsHTML;
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+
+
 // Card Function Section--
 fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts')
   .then(data => {
@@ -10,11 +61,11 @@ fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts')
           <img src="${
             values.cover_image
           }" alt="img" class="cover_image rounded-2xl pb-2" />
-          
-          <h2 class="posted-date my-2">
+
+          <p class="posted-date my-2">
             <i class="fa-regular fa-calendar"></i>
             ${values.author.posted_date || 'No publish date'}
-          </h2>
+          </p>
 
           <h1 class="title font-bold py-2">${values.title}</h1>
           <p class="py-2">${values.description}</p>
