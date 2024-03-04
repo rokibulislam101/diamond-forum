8a7d35a6fcdd5c5196a36843a6e81a39246d885d
MD Rokibul Islam Mollah <rokibulislamalways101@gmail.com>
2024-03-04 07:51:09 +0600

    Fourth Commit

--- js/ticket.js
+++ js/ticket.js
@@ -24,6 +24,7 @@ fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
             </div>
             <h2 class="font-bold py-2">${post.title}</h2>
             <p class="py-2">${post.description}</p>
+            <hr class="border-dashed">
             <div class="flex justify-between items-center">
               <div class="flex gap-5">
                 <p><i class="fa-regular fa-message me-3"></i>${
@@ -34,18 +35,61 @@ fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
                   post.posted_time
                 } min</p>
               </div>
-              <button class="bg-green-400 text-white p-2 flex items-center rounded-full me-0"><i class="fa-regular fa-envelope-open"></i></button>
+              <button class="show-btn btn bg-green-400 text-white p-2 btn-circle flex justify-center items-center rounded-full" id="addButton"><i class="fa-regular fa-envelope-open"></i></button>
             </div>
-  
-
           </div>
         </div>`;
     });
     document.getElementById('posts').innerHTML = postsHTML;
+
+    // Add event listener after setting the innerHTML
+    addEventListenersToButtons();
   })
-  .catch(error => {
-    console.log(error);
+  .catch(err => {
+    console.log(err);
   });
+
+function addEventListenersToButtons() {
+  const allBtn = document.getElementsByClassName('show-btn');
+  let count = 0;
+  for (const btn of allBtn) {
+    btn.addEventListener('click', function (e) {
+      count = count + 1;
+      document.getElementById('read-count').innerText = `(${count})`;
+    });
+  }
+}
+
+
+
+
+
+
+
+// // Additional-Info Function Section--
+// document.getElementById('addButton').addEventListener('click', () => {
+//   fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
+//     .then(response => {
+//       return response.json();
+//     })
+//     .then(postsData => {
+//       let postsHTML = '';
+//       postsData.posts.map(post => {
+//         postsHTML += `<div class="flex bg-white justify-between p-5 rounded-2xl my-3">
+//                 <h2 class="font-bold py-2">${post.title}</h2>
+//                 <p><i class="fa-regular fa-eye me-3"></i>${post.view_count}</p>
+//               </div>`;
+//       });
+//       document.getElementById('additional-info').innerHTML = postsHTML;
+//       document.getElementById('additional-info').style.display = 'block';
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// });
+
+
+
 
 
 
